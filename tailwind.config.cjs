724168e7034cb54ed8652de5d55b10b704b00e6a
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2023-08-05 16:31:10 +0530

    Fix dark mode classes and design

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,astro}"],
-  darkMode: "media",
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
