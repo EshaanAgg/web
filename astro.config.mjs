e253a10a7abe293b2c361933efb3a26b32224afd
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2023-08-05 13:49:58 +0530

    Update lockfile and deployment URLS

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ export default defineConfig({
   experimental: {
     assets: true,
   },
-  site: "https://www.kevinzunigacuellar.com/",
+  site: "https://eshaanagg.netlify.app/",
   image: {
     service: sharpImageService(),
   },
